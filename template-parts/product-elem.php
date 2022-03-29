ca0d0d93728d36338e141b71976bb335abb54ce4
asmi046 <asmi046@gmail.com>
2022-03-30 00:14:26 +0300

    Нет фото

--- template-parts/product-elem.php
+++ template-parts/product-elem.php
@@ -1,7 +1,7 @@
 <div class="prodCard__column">
   <div class="prodCard__box">
     <div class="prodCard__box-img">
-      <img src="<?php echo get_template_directory_uri();?>/img/delivery/01.jpg" alt="">
+      <img src="<?php  $imgTm = get_the_post_thumbnail_url( get_the_ID(), "tominiatyre" ); echo empty($imgTm)?get_bloginfo("template_url")."/img/no-photo.jpg":$imgTm; ?>" alt="">
     </div>
     <div class="prodCard__box-descp">
       <h4 class="prodCard__box-descp-title"><? the_title();?></h4>
@@ -22,16 +22,3 @@
     </div>
   </div>
 </div>			
-
-
-<!-- <div class="prod-card__body d-flex">
-	<a href="<?echo get_the_permalink(get_the_ID());?>" class="prod-card__link">
-		<img src="<?php  $imgTm = get_the_post_thumbnail_url( get_the_ID(), "tominiatyre" ); echo empty($imgTm)?get_bloginfo("template_url")."/img/no-photo.jpg":$imgTm; ?>" alt="<? the_title();?>"> 
-	</a>
-
-	<div class="prod-card__text d-flex">
-		<h4><? the_title();?></h4>
-		<p class="prod-card__price"><?echo carbon_get_post_meta(get_the_ID(),"offer_price"); ?> руб.</p>
-	</div>
-	<a href="<?echo get_the_permalink(get_the_ID());?>" class="prod-card__btn btn"><? echo pll_e("Подробнее") ?></a> 
-</div> -->
